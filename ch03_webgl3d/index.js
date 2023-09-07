8602b18e50fedd9cf6105013e9ff9e5ccf7c4da3
Jan Horák <jhorak@kambrno.local>
2023-09-07 16:18:07 +0200

    Make node app and add perspective view, and another rotation and color picker

--- ch03_webgl3d/index.js
+++ ch03_webgl3d/index.js
@@ -1,5 +1,6 @@
 function main() {
   /*========== Create a WebGL Context ==========*/
+  const colorPicker = document.querySelector('#colorPicker');
   const canvas = document.querySelector('#c');
   const gl = canvas.getContext('webgl');
   function checkContext(gl) {
@@ -13,22 +14,112 @@ function main() {
   checkContext(gl);
 
   /*========== Define and Store the Geometry ==========*/
-  /*====== Define front-face vertices ======*/
-  const squares = [
-    //front face
-    -0.3, -0.3, -0.3, 0.3, -0.3, -0.3, 0.3, 0.3, -0.3, -0.3, -0.3, -0.3, -0.3, 0.3, -0.3, 0.3, 0.3, -0.3,
-    // back face
-    -0.2, -0.2, 0.3, 0.4, -0.2, 0.3, 0.4, 0.4, 0.3, -0.2, -0.2, 0.3, -0.2, 0.4, 0.3, 0.4, 0.4, 0.3,
 
-    // top face
-    -0.3, 0.3, -0.3, 0.3, 0.3, -0.3, -0.2, 0.4, 0.3, 0.4, 0.4, 0.3, 0.3, 0.3, -0.3, -0.2, 0.4, 0.3,
+  const squares = [
+    // Front face
+    -0.5, -0.5, 0.5,  // Vertex 1
+     0.5, -0.5, 0.5,  // Vertex 2
+     0.5,  0.5, 0.5,  // Vertex 3
+    -0.5, -0.5, 0.5,  // Vertex 1
+     0.5,  0.5, 0.5,  // Vertex 3
+    -0.5,  0.5, 0.5,  // Vertex 4
+  
+    // Back face
+    -0.5, -0.5, -0.5,  // Vertex 5
+     0.5, -0.5, -0.5,  // Vertex 6
+     0.5,  0.5, -0.5,  // Vertex 7
+    -0.5, -0.5, -0.5,  // Vertex 5
+     0.5,  0.5, -0.5,  // Vertex 7
+    -0.5,  0.5, -0.5,  // Vertex 8
+  
+    // Top face
+    -0.5,  0.5,  0.5,  // Vertex 4
+     0.5,  0.5,  0.5,  // Vertex 3
+     0.5,  0.5, -0.5,  // Vertex 7
+    -0.5,  0.5,  0.5,  // Vertex 4
+     0.5,  0.5, -0.5,  // Vertex 7
+    -0.5,  0.5, -0.5,  // Vertex 8
+  
+    // Bottom face
+    -0.5, -0.5,  0.5,  // Vertex 1
+     0.5, -0.5,  0.5,  // Vertex 2
+     0.5, -0.5, -0.5,  // Vertex 6
+    -0.5, -0.5,  0.5,  // Vertex 1
+     0.5, -0.5, -0.5,  // Vertex 6
+    -0.5, -0.5, -0.5,  // Vertex 5
+  
+    // Right face
+     0.5, -0.5,  0.5,  // Vertex 2
+     0.5,  0.5,  0.5,  // Vertex 3
+     0.5,  0.5, -0.5,  // Vertex 7
+     0.5, -0.5,  0.5,  // Vertex 2
+     0.5,  0.5, -0.5,  // Vertex 7
+     0.5, -0.5, -0.5,  // Vertex 6
+  
+    // Left face
+    -0.5, -0.5,  0.5,  // Vertex 1
+    -0.5,  0.5,  0.5,  // Vertex 4
+    -0.5,  0.5, -0.5,  // Vertex 8
+    -0.5, -0.5,  0.5,  // Vertex 1
+    -0.5,  0.5, -0.5,  // Vertex 8
+    -0.5, -0.5, -0.5   // Vertex 5
   ];
+
 
   const squareColors = [
-    0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
-    1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
-    0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
+    // Front face (blue)
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+  
+    // Back face (red)
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 1.0,
+  
+    // Top face (green)
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+  
+    // Bottom face (yellow)
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0,
+  
+    // Right face (purple)
+    1.0, 0.0, 1.0, 1.0,
+    1.0, 0.0, 1.0, 1.0,
+    1.0, 0.0, 1.0, 1.0,
+    1.0, 0.0, 1.0, 1.0,
+    1.0, 0.0, 1.0, 1.0,
+    1.0, 0.0, 1.0, 1.0,
+  
+    // Left face (cyan)
+    0.0, 1.0, 1.0, 1.0,
+    0.0, 1.0, 1.0, 1.0,
+    0.0, 1.0, 1.0, 1.0,
+    0.0, 1.0, 1.0, 1.0,
+    0.0, 1.0, 1.0, 1.0,
+    0.0, 1.0, 1.0, 1.0,
   ];
+  
+
+
+
+
 
   /*====== Define front-face buffer ======*/
   // buffer
@@ -40,6 +131,30 @@ function main() {
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(squareColors), gl.STATIC_DRAW);
 
+
+  function updateCubeColor() {
+    const r = parseInt(selectedColor.slice(1, 3), 16) / 255.0;
+    const g = parseInt(selectedColor.slice(3, 5), 16) / 255.0;
+    const b = parseInt(selectedColor.slice(5, 7), 16) / 255.0;
+    
+  
+    // Update the squareColors array with the new color and random alpha values for each face
+    for (let i = 0; i < squareColors.length; i += 24) {
+      const randomBlue = Math.random(); // Generate a random alpha value for each face
+      
+      for (let j = 0; j < 6; j++) {
+        squareColors[i + (j * 4)] = r;
+        squareColors[i + (j * 4) + 1] = g;
+        squareColors[i + (j * 4) + 2] = randomBlue;
+      }
+    }
+  
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(squareColors), gl.STATIC_DRAW);
+  }
+  
+
+
   /*========== Shaders ==========*/
   /*====== Define shader source ======*/
   const vsSource = /*glsl*/ `
@@ -47,11 +162,10 @@ function main() {
     attribute vec4 aVertexColor;
     uniform mat4 uModelViewMatrix;
     varying lowp vec4 vColor;
-
-
+    uniform mat4 uProjectionMatrix;
 
     void main() {
-    gl_Position = uModelViewMatrix * aPosition;
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aPosition;
     vColor = aVertexColor;
     }  
   `;
@@ -99,7 +213,7 @@ function main() {
   let then = 0;
 
   function render(now) {
-    now *= 0.0001;
+    now *= 0.001;
     let deltaTime = now - then;
     then = now;
 
@@ -117,11 +231,30 @@ function main() {
     gl.vertexAttribPointer(colorAttribLocation, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(colorAttribLocation);
 
+    // Projection Matrix
+    const projMatrixLocation = gl.getUniformLocation(program, 'uProjectionMatrix');
+    const fieldOfView = 45 * Math.PI / 180; // in radians
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100.0;
+    const projectionMatrix = glMatrix.mat4.create();
+    
+    glMatrix.mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar)
+    
+    gl.uniformMatrix4fv(projMatrixLocation, false, projectionMatrix)
+    
+    // Rotation Matricies
     const modelMatrixLocation = gl.getUniformLocation(program, 'uModelViewMatrix');
     const modelViewMatrix = glMatrix.mat4.create();
+    
+    glMatrix.mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -5.0])
     glMatrix.mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 0, 1]);
+    glMatrix.mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation,[0, 1, 0]); // axis to rotate around (X)
 
     gl.uniformMatrix4fv(modelMatrixLocation, false, modelViewMatrix);
+
+
+    // // Translation for culling
 
     /*========== Drawing ========== */
     gl.clearColor(1, 1, 1, 1);
@@ -133,11 +266,17 @@ function main() {
 
     const mode = gl.TRIANGLES;
     const first = 0;
-    const count = 18;
+    const count = 36;
     gl.drawArrays(mode, first, count);
     cubeRotation += deltaTime;
     requestAnimationFrame(render);
   }
+
+
+  colorPicker.addEventListener('input', (event) => {
+    selectedColor = event.target.value;
+    updateCubeColor();
+  });
 
   requestAnimationFrame(render);
 }
