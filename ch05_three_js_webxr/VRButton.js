d765654881f9b78c1abd6e3e35f1d75683f73420
Jan Horák <jhorak@kambrno.local>
2023-09-12 16:39:26 +0200

    MVP

--- ch05_three_js_webxr/VRButton.js
+++ ch05_three_js_webxr/VRButton.js
@@ -1,159 +1,99 @@
-
-// const VRButton = {
-//     createButton: function (gl, options) {
-//         if (options && options.referenceSpaceType) {
-//             gl.xr.setReferenceSpaceType(options.referenceSpaceType)
-//         }
-
-//         function EnterVR() {
-//             button.innerHTML = 'Enter VR'
-//             let currentSession = null;
-//             function onSessionStarted(session) {
-//                 session.addEventListener('end', onSessionEnded)
-//                 gl.xr.setSession(session)
-
-//                 button.textContent = 'ExitVR'
-//                 currentSession = session
+const VRButton = {
+    createButton: function (gl, options) {
+        if (options && options.referenceSpaceType) {
+            gl.xr.setReferenceSpaceType(options.referenceSpaceType)
+        }
 
 
-//             }
-        
-//             function onSessionEnded(/*event*/) {
-//                 currentSession.removeEventListener('end', onSessionEnded);
-//                 button.textContent = 'ExnterVR'
-//                 currentSession = null
-        
-//             }
-        
-        
-//             function setupWebGLLayer() {
-        
-//             }
-        
-        
-        
-//             button.onclick = () => {
-//                 if (currentSession === null) {
-//                     let sessionInit = {
-//                         optionalFeatures: ["local-floor", "bounded-floor"]
-//                     };
-//                 navigator.xr
-//                             .requestSession('immersive-vr', sessionInit)
-//                             .then(onSessionStarted);
-//                 }
-//                 else {
-//                     currentSession.end();
-//                 }
-//             }
-//         }    
+        function buildButton(){
+            let enterVRButton = document.createElement('button')
+            enterVRButton.textContent = "Enter VR";
+            enterVRButton.setAttribute("id", "btn");
+            return enterVRButton
+        }
 
-                
-//         function NotFound(){
-//             console.log('immersive-vr mode not found')
-//         }
-
-//         if (navigator.xr) {
-//             var button = document.createElement("button");
-//             navigator.xr.isSessionSupported('immersive-vr')
-//                     .then((suported) => {
-//                         if (suported) { EnterVR() 
-//                             button.classList.add("vr-button")} 
-//                         else { NotFound() }
-//                     })
-//             button.setAttribute('id', 'btn')
-//             return button
-//         } else {
-//             if (window.isSecureContext === false) {
-//                 console.log('WebXR needs HTTPS');
-//             } else {
-//                 console.log('WebXR not available');
-//             }
-//             return;
-//         }
-//     }
-// }
-
-
-
-// export {VRButton};
-
-function perpareVR(button){
-    // create async button for user to choose to go into VR
-    button.addEventListener('click', () => {
-
-// DODELAT
-        if (!xrSession) {
-            // request session from device
-            let sessionInit = {optionalFeatures: ["local-floor", "bounded-floor"]};
-            navigator.xr.requestSession('immersive-vr', sessionInit)
-            // retruns async promise object Sesssion
-            .then((session) => {
+        function perpareVR(button){
+            let currentSession = null;
+            function onSessionStarted(session){
+                // callback to end session
                 session.addEventListener('end', onSessionEnded)
-                global.xr.setSession(session);
-                button.textContent = "Exit VR"
+
+                // mutate button content
+                button.textContent = 'Exit XR'
+                // set current session - WHY?
                 currentSession = session
-            })
-        }
 
-    })
-}
+                setupWebGLLayer()
+                    .then(() => {
+                       gl.xr.setSession(currentSession)     
+                    })
 
-
-function NotFound(){
-    
-
-}
-
-
-
-function queryXRSesh() {
-        // The if statement checks if the browser’s navigator property contains an XR object
-    if (navigator.xr){
-        
-        // check if 'immersive-vr' mode is suported - here we could also check if 'inline' is suported and display differnet buttons
-        // is session suported returns bool - supported
-        navigator.xr.isSessionSupported('immersive-vr')
-        .then(function(isSuported){
-            if (isSuported) {
-                let vrButton = document.createElement('button')
-                vrButton.textContent = "Enter VR";
-                vrButton.disabled = false;
-                perpareVR(vrButton)
-            } else {
-                // the browser supports immersive-vr sesh however the device does not 
-                console.log('immersive-vr mode not found');
             }
-        })
+
+            function onSessionEnded(){
+                currentSession.removeEventListener('end', onSessionEnded)
+                button.textContent('Enter VR')
+                currentSession = null
+            }
+
+
+            function setupWebGLLayer() {
+                var glContext = gl.getContext();
+                return glContext.makeXRCompatible().then(() => {
+                    currentSession.updateRenderState( {baseLayer: new XRWebGLLayer(currentSession, glContext) });
+                });
+            }
+
+
+            button.addEventListener('click', () => {
+                if (currentSession === null) {
+                    let sessionInit = { optionalFeatures: ["local-floor"]}
+                    navigator.xr
+                        .requestSession('immersive-vr', sessionInit)
+                        .then(onSessionStarted)
+                } else {
+                    currentSession.end()
+                }
+
+            })  
+
+        }
+
+
+        function vrNotFound(){
+            console.log('immersive-vr mode not found')
+        }
 
         
-    } else {
-        if(window.isSecureContext() === false) {
-            console.log('WebXR needs HTTPS')
+        // The if statement checks if the browser’s navigator property contains an XR object
+        if (navigator.xr){
+            var button = buildButton()
+            // check if 'immersive-vr' mode is suported - here we could also check if 'inline' is suported and display differnet buttons
+            // is session suported returns bool - supported
+            navigator.xr.isSessionSupported('immersive-vr')
+                        .then(function(isSuported){
+                            if (isSuported) {
+                                perpareVR(button)
+                            } else {
+                                // the browser supports immersive-vr sesh however the device does not 
+                                vrNotFound();
+                            }
+                        })
+            return button
         } else {
-            console.log('WebXR not available')
-            // keep the three scene as a Desktop VR
+            if(window.isSecureContext() === false) {
+                console.log('WebXR needs HTTPS')
+            } else {
+                console.log('WebXR not available')
+                // keep the three scene as a Desktop VR
+            }
+            return;
         }
+
+        return VRButton
+
     }
-
-
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export {VRButton}
 
--- ch05_three_js_webxr/main.js
+++ ch05_three_js_webxr/main.js
@@ -22,48 +22,65 @@ function init() {
     scene.fog = new THREE.Fog('grey', 1, 90)
 
 
+
+    function createCamera(){
+        camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000) // define camera 
+        scene.add(camera) // add to the scene
+        camera.position.set(0,3,20) 
+        return camera
+    }
     // create camera
-    camera = new THREE.PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000) // define camera 
-    scene.add(camera) // add to the scene
-    // modify existing object
-    camera.position.set(0,8,30) 
-
-
+    camera = createCamera()
+    let group = new THREE.Group()
+    group.add(camera)
+    group.position.set(0, 0, 0)
+    scene.add(group)
 
  // Objects 
     // loaders
     const textureLoader = new THREE.TextureLoader();
 
  // create the cube
-    const cubeGeometry = new THREE.BoxGeometry(1,1,1)
+    const cubeGeometry = new THREE.BoxGeometry(3,3,3)
     const cubeMaterial = new THREE.MeshPhongMaterial({
-        color:'pink'
+        color:'red'
     })
     cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
     scene.add(cube)
-    cube.position.set(1, 1 , 0);
+    cube.position.set(5, 1, 1);
 
  // Create the Sphere
     const sphereGeometry = new THREE.SphereGeometry(3, 32, 32)
     const sphereNormalMap = textureLoader.load('./textures/sphere_normal.png')
+    sphereNormalMap.wrapS = THREE.RepeatWrapping;
+    sphereNormalMap.wrapT = THREE.RepeatWrapping;
     const sphereMaterial = new THREE.MeshStandardMaterial({
-        color:'pink',
+        color:'white',
         normalMap: sphereNormalMap
     })
     sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
     scene.add(sphere)
-    sphere.position.set(1, 1, 0)
+    sphere.position.set(-5, 1, 1)
  // Create the upright plane
 
     const planeGeom = new THREE.PlaneGeometry(256, 128)
     const planeMap = textureLoader.load('./textures/pebbles.jpg')
+    planeMap.wrapS = THREE.RepeatWrapping;
+    planeMap.wrapT = THREE.RepeatWrapping;
+    planeMap.repeat.set(16, 16);
+    planeMap.minFilter = THREE.NearestFilter;
+    planeMap.anisotropy = gl.capabilities.getMaxAnisotropy();
+
     const planeNormalMap = textureLoader.load('./textures/pebbles_normal.png')
+    planeNormalMap.wrapS = THREE.RepeatWrapping;
+    planeNormalMap.wrapT = THREE.RepeatWrapping;
+    planeNormalMap.minFilter = THREE.NearestFilter;
+    planeNormalMap.repeat.set(16, 16);
 
     const planeMaterial = new THREE.MeshStandardMaterial({
         map: planeMap,
         side:THREE.DoubleSide,
         normalMap: planeNormalMap,
-
     })
 
     plane = new THREE.Mesh(planeGeom, planeMaterial)
@@ -90,53 +107,43 @@ function init() {
 }
 
 
-
-
-
 function animate() {
-    
-    function render(time) {
-        time *= 0.001;
-        if (resizeDisplay) {
-            camera.aspect = window.innerWidth / window.
-        innerHeight;
-            camera.updateProjectionMatrix();
-       }
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        cube.rotation.z += 0.01;
-        sphere.rotation.x += 0.01;
-        sphere.rotation.y += 0.01;
-        sphere.rotation.y += 0.01;
-        light.position.x = 20*Math.cos(time);
-        light.position.y = 20*Math.sin(time);
-        gl.render(scene, camera);
-       }
-
-
-       function resizeDisplay() {
-        const canvas = gl.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width != width || canvas.
-       height != height;
-        if (needResize) {
-        gl.setSize(width, height, false);
-        }
-        return needResize;
-       }
-
-
-       if (resizeDisplay(gl)){
-        const canvas = gl.domElement
-        camera.aspect = canvas.clientWidth / canvas.clientHeight
-        camera.updateProjectionMatrix();
-    }
-
-    
+   
     gl.setAnimationLoop(render)
 
-
-
 }
+
+function render(time) {
+    time *= 0.001;
+    if (resizeDisplay) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+   }
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+    sphere.rotation.y += 0.01;
+
+    light.position.x = 20*Math.cos(time);
+    light.position.y = 20*Math.sin(time);
+    gl.render(scene, camera);
+}
+
+
+function resizeDisplay() {
+    const canvas = gl.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width != width || canvas.
+   height != height;
+    if (needResize) {
+    gl.setSize(width, height, false);
+    }
+    return needResize;
+   }
+
+
 
